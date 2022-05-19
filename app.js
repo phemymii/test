df93ba50dc3085b2167b595b8b612d684211da72
phemymii <olayiwolafemmy@yahoo.com>
2022-05-19 10:29:01 +0100

    the second commit in femi B

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (request, response)=>{
     return response.json({message: 'done'})
 })
 
+app.post('/', (request, response)=>{
+    return response.json({message: 'done'})
+})
+
 const server = app.listen(4000, ()=>{
     console.log('server running');
 })
