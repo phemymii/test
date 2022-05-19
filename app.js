71dd57b9f09581e829fb1c24ea1fd096bd9d5153
phemymii <olayiwolafemmy@yahoo.com>
2022-05-19 10:48:09 +0100

    the third commit in femi C

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const { response } = require('express');
 
 const app = require('express')()
 
-app.get('/', (request, response)=>{
-    return response.json({message: 'done'})
-})
+
 
 app.post('/', (request, response)=>{
     return response.json({message: 'done'})
